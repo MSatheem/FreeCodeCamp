2cba5e618f5394a064d2231a5c777406b686fc48
MSatheem <138306393+MSatheem@users.noreply.github.com>
2025-09-29 21:08:43 +0530

    Fix
     first element in the list not included
    
    Test case updated

--- September2025/Sept29/Sept29.py
+++ September2025/Sept29/Sept29.py
@@ -2,7 +2,7 @@ import unittest
 
 def get_longest_word(sentence):
     list = sentence.strip().replace('.','').split(" ")
-    longestword = list[1]
+    longestword = list[0]
     for i in list:
         if(len(i) > len(longestword)):
             longestword = i
@@ -18,6 +18,9 @@ class TestFunction(unittest.TestCase):
 
     def test3(self):
         self.assertEqual(get_longest_word("This sentence has multiple long words."), "sentence")
+    
+    def test4(self):
+        self.assertEqual(get_longest_word("Longestishere This sentence has multiple long words."), "Longestishere")
 
 if __name__ == '__main__':
     unittest.main()
