9ca0b0b020a23d7425f5bba531638556e6e627fa
MSatheem <138306393+MSatheem@users.noreply.github.com>
2025-10-05 15:45:20 +0530

    TestCase 1 and 2

---
+++ October/Oct05/Oct05.py
@@ -0,0 +1,18 @@
+import unittest
+
+def has_exoplanet(readings):
+    
+    
+
+
+print(has_exoplanet("FGFFCFFGG"))
+
+# class TestCase(unittest.TestCase):
+#     def test1(self):
+#         self.assertEqual(has_exoplanet("665544554"), False)
+
+#     def test2(self):
+#         self.assertEqual(has_exoplanet("FGFFCFFGG"), True)
+
+# if __name__ == '__main__':
+#     unittest.main()
